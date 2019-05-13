ccd6a729c9c81b90a7d3938dc4254e775c8e413b
PedroMalho <pmvmalhotera@gmail.com>
2019-05-13 02:26:06 +0100

    Exercicios do projeto parte A

--- server.js
+++ server.js
@@ -39,19 +39,58 @@ app.post('/', function (req, res) {
 });
 
 //Seleccionar todas as fotografias de um uploader
-app.get('/:uploader', function(req, res) {
+// app.get('/:uploader', function (req, res) {
+//     var foto = JSON.parse(readFile('./photos.json'));
+
+//     var fotoKeys = Object.keys(foto);
+//     var fotoLength = fotoKeys.length;
+
+//     var uploader = req.params.uploader;
+//     var resul = [];
+
+//     for (var i = 1; i <= fotoLength; i++) {
+//         if (foto['fotografia' + i].uploader == uploader) {
+//             resul.push(foto['fotografia' + i]);
+//         }
+//     };
+//     console.log(resul);
+//     res.send(resul);
+// });
+
+//Incrementar o numero de likes e atualizar o ficheiro
+app.post('/', function (req, res) {
     var foto = JSON.parse(readFile('./photos.json'));
 
     var fotoKeys = Object.keys(foto);
     var fotoLength = fotoKeys.length;
 
-    var uploader = req.params.uploader;
-
-    for (var i = 1; i < fotoLength; i++) {
-        if (foto['fotografia'+i].uploader == uploader) {
-            var resul = resul + foto['fotografia'+i];
-        } 
+    for (var i = 1; i <= fotoLength; i++) {
+        foto['fotografia' + i]['Likes'] += 1;
     };
-    console.log(resul);
-    res.send(resul);
+
+    fs.writeFileSync('./photos.json', JSON.stringify(foto, null, 4));
+    res.send(foto);
+});
+
+//Listar todas as fotografias por tags
+app.get('/:tag1/:tag2', function(req, res) {
+    var foto = JSON.parse(readFile('./photos.json'));
+
+    var fotoKeys = Object.keys(foto);
+    var fotoLength = fotoKeys.length;
+
+    var resul = [];
+
+    for (var i = 1; i <= fotoLength; i++) {
+        for ( var j = 0; j <= foto['fotografia' + i]['tags'].length; j++ )
+            if ((foto['fotografia' + i]['tags'][j] == req.params.tag1) || foto['fotografia' + i]['tags'][j] == req.params.tag2)  {
+                resul.push(foto['fotografia' + i]);
+            };
+    };
+
+    var resulFinal = resul.filter(function (fotografia, index) {
+        return resul.indexOf(fotografia) === index;
+    });
+
+    res.send(resulFinal);
 });

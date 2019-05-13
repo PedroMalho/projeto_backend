5570f4a3e28890c70f8ea94d396409992b31ad4c
PedroMalho <pmvmalhotera@gmail.com>
2019-05-13 23:50:28 +0100

    Projeto BackEnd Parte A Final

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const uuid = require('uuid/v1');
+const uuidv1 = require('uuid/v1'); //Gera uuid
 const app = express();
 const fs = require('fs');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var uui = uuid();
 
 //Função para obter o length do ficheiro photos.json
 function photosLength(fileName) {
@@ -16,64 +16,76 @@ function photosLength(fileName) {
     return fotoLength;
 };
 
+
 const port = 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
 
 function readFile(fileName) {
     var file = fs.readFileSync(fileName, 'utf-8');
     return file;
 }
 
-// Lista todas as fotografias existentes no ficheiro photos.json
+
+//Parte A alienea a) -> Lista todas as fotografias existentes no ficheiro photos.json
 app.get('/', function (req, res) {
     res.send(JSON.parse(readFile('./photos.json')));
 });
 
-//Adicionar nova imagem e atualizar o ficheiro
+
+// //Parte A alinea b) -> Adicionar nova imagem e atualizar o ficheiro
 app.post('/', function (req, res) {
-    var foto = JSON.parse(readFile('./photos.json'));
+    var foto = JSON.parse(readFile("./photos.json"));
 
-    foto['fotografia' + (photosLength(foto) + 1)] = req.body;
-    foto['fotografia' + (photosLength(foto) + 1)].id = uui;
+    var fotoKeys = Object.keys(foto);
+    var fotoLength = fotoKeys.length;
 
-    fs.writeFileSync('./photos.json', JSON.stringify(foto, null, 4));
+    foto['fotografia' + (fotoLength + 1)] = req.body;
+    foto['fotografia' + (fotoLength + 1)].id = uuidv1();
 
-    console.log('fotografia' + (photosLength(foto) + 1) + " adicionada com sucesso.");
-    res.send(foto);
+    var conteudo = JSON.stringify(foto, null, 4);
+    fs.writeFile('./photos.json', conteudo, function (err) {
+        if (err) throw err;
+        console.log('fotografia' + (fotoLength + 1) + ' guardada com sucesso!');
+    });
 });
 
-//Seleccionar todas as fotografias de um uploader
-app.get('/:uploader', function (req, res) {
+
+//Parte A alinea c) -> Seleccionar todas as fotografias de um uploader
+app.get('/uploader/:nome', function (req, res) {
     var foto = JSON.parse(readFile('./photos.json'));
 
-    var uploader = req.params.uploader;
-    var resul = [];
-// !!!!!!!!!!!!!!!!!!!!! VERIFICAR O RESUL PARA SER COMO ALINEA E !!!!!!!!!!!!!!!!!!!!!!!!
+    var uploader = req.params.nome.toLowerCase(); //Passa para letras minusculuas
+
+    //Objeto vazio para guardar os resultados
+    var resul = {};
+
     for (var i = 1; i <= photosLength(foto); i++) {
-        if (foto['fotografia' + i].uploader == uploader) {
-            resul.push(foto['fotografia' + i]);
+        if (foto['fotografia' + i].uploader.toLowerCase() == uploader) {
+            resul['fotografia' + i] = foto['fotografia' + i];
         }
     };
 
     res.send(resul);
 });
 
-//Incrementar o numero de likes e atualizar o ficheiro
-app.post('/:title', function (req, res) {
+
+//Parte A alinea d) -> Incrementar o numero de likes e atualizar o ficheiro
+app.post('/like/:id', function (req, res) {
     var foto = JSON.parse(readFile('./photos.json'));
 
+    //Ciclo que percorre o ficheiro photos.json, encontra o id especifico, incrementa os likes e devolve o resultado atualizado na resposta
     for (var i = 1; i <= photosLength(foto); i++) {
-        if (foto['fotografia' + i]['Title'] == req.params.title) {
+        if (foto['fotografia' + i]['id'] == req.params.id) {
             foto['fotografia' + i]['Likes'] += 1;
             res.send(foto['fotografia' + i]);
+            fs.writeFileSync('./photos.json', JSON.stringify(foto, null, 4));
         }
     };
-// !!!!!!!!!!!!!!!!!!!!!!!  ALTERAR PARA SER COM O ID EM VEZ DO TITULO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    fs.writeFileSync('./photos.json', JSON.stringify(foto, null, 4));
-
 });
 
-//Alinea E)
+
+//Parte A alinea e) -> 
 app.get('/search', function (req, res) {
     var foto = JSON.parse(readFile('./photos.json'));
 
@@ -88,7 +100,7 @@ app.get('/search', function (req, res) {
         for (var j = 0; j <= foto['fotografia' + i]['tags'].length; j++) {
             for (var k = 0; k < q.length; k++) {
                 if (foto['fotografia' + i]['tags'][j] == q[k]) {
-                    resul['fotografia' + i] = foto['fotografia' + i];
+                    resul['fotografia' + i] = foto['fotografia' + i]; // Guarda a fotografia dentro do objecto criado anteriormente evitando resultados repetidos
                 };
             };
         };

f90d0066fa8777f6ecb7f7027ca1d7722dc42cd7
PedroMalho <pmvmalhotera@gmail.com>
2019-05-13 13:07:48 +0100

    Mudanças feitas na aula de SGBD

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 var uui = uuid();
 
+//Função para obter o length do ficheiro photos.json
+function photosLength(fileName) {
+    var fotoKeys = Object.keys(fileName);
+    var fotoLength = fotoKeys.length;
+    return fotoLength;
+};
+
 const port = 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
@@ -26,71 +33,66 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
     var foto = JSON.parse(readFile('./photos.json'));
 
-    var fotoKeys = Object.keys(foto);
-    var fotoLength = fotoKeys.length;
-
-    foto['fotografia' + (fotoLength + 1)] = req.body;
-    foto['fotografia' + (fotoLength + 1)].id = uui;
+    foto['fotografia' + (photosLength(foto) + 1)] = req.body;
+    foto['fotografia' + (photosLength(foto) + 1)].id = uui;
 
     fs.writeFileSync('./photos.json', JSON.stringify(foto, null, 4));
 
-    console.log('fotografia' + (fotoLength + 1) + " adicionada com sucesso.");
+    console.log('fotografia' + (photosLength(foto) + 1) + " adicionada com sucesso.");
     res.send(foto);
 });
 
 //Seleccionar todas as fotografias de um uploader
-// app.get('/:uploader', function (req, res) {
-//     var foto = JSON.parse(readFile('./photos.json'));
-
-//     var fotoKeys = Object.keys(foto);
-//     var fotoLength = fotoKeys.length;
-
-//     var uploader = req.params.uploader;
-//     var resul = [];
-
-//     for (var i = 1; i <= fotoLength; i++) {
-//         if (foto['fotografia' + i].uploader == uploader) {
-//             resul.push(foto['fotografia' + i]);
-//         }
-//     };
-//     console.log(resul);
-//     res.send(resul);
-// });
-
-//Incrementar o numero de likes e atualizar o ficheiro
-app.post('/', function (req, res) {
+app.get('/:uploader', function (req, res) {
     var foto = JSON.parse(readFile('./photos.json'));
 
-    var fotoKeys = Object.keys(foto);
-    var fotoLength = fotoKeys.length;
-
-    for (var i = 1; i <= fotoLength; i++) {
-        foto['fotografia' + i]['Likes'] += 1;
+    var uploader = req.params.uploader;
+    var resul = [];
+// !!!!!!!!!!!!!!!!!!!!! VERIFICAR O RESUL PARA SER COMO ALINEA E !!!!!!!!!!!!!!!!!!!!!!!!
+    for (var i = 1; i <= photosLength(foto); i++) {
+        if (foto['fotografia' + i].uploader == uploader) {
+            resul.push(foto['fotografia' + i]);
+        }
     };
 
-    fs.writeFileSync('./photos.json', JSON.stringify(foto, null, 4));
-    res.send(foto);
+    res.send(resul);
 });
 
-//Listar todas as fotografias por tags
-app.get('/:tag1/:tag2', function(req, res) {
+//Incrementar o numero de likes e atualizar o ficheiro
+app.post('/:title', function (req, res) {
     var foto = JSON.parse(readFile('./photos.json'));
 
-    var fotoKeys = Object.keys(foto);
-    var fotoLength = fotoKeys.length;
+    for (var i = 1; i <= photosLength(foto); i++) {
+        if (foto['fotografia' + i]['Title'] == req.params.title) {
+            foto['fotografia' + i]['Likes'] += 1;
+            res.send(foto['fotografia' + i]);
+        }
+    };
+// !!!!!!!!!!!!!!!!!!!!!!!  ALTERAR PARA SER COM O ID EM VEZ DO TITULO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    fs.writeFileSync('./photos.json', JSON.stringify(foto, null, 4));
 
-    var resul = [];
+});
 
-    for (var i = 1; i <= fotoLength; i++) {
-        for ( var j = 0; j <= foto['fotografia' + i]['tags'].length; j++ )
-            if ((foto['fotografia' + i]['tags'][j] == req.params.tag1) || foto['fotografia' + i]['tags'][j] == req.params.tag2)  {
-                resul.push(foto['fotografia' + i]);
+//Alinea E)
+app.get('/search', function (req, res) {
+    var foto = JSON.parse(readFile('./photos.json'));
+
+    //Objecto vazio onde serão guardados os resultados
+    var resul = {};
+
+    //Cria um array a partir das tags recebidas
+    var q = req.query.tags.split(" ");
+
+    //Ciclos que percorrem os arrays para encontrar as fotografias com as tags dadas
+    for (var i = 1; i <= photosLength(foto); i++) {
+        for (var j = 0; j <= foto['fotografia' + i]['tags'].length; j++) {
+            for (var k = 0; k < q.length; k++) {
+                if (foto['fotografia' + i]['tags'][j] == q[k]) {
+                    resul['fotografia' + i] = foto['fotografia' + i];
+                };
             };
+        };
     };
 
-    var resulFinal = resul.filter(function (fotografia, index) {
-        return resul.indexOf(fotografia) === index;
-    });
-
-    res.send(resulFinal);
+    res.send(resul);
 });
